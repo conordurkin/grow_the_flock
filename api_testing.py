e68bcdaa8efd86e717a638a4937c91b03e6ffcb9
Conor Durkin <conordurkin@me.com>
2021-01-10 19:26:48 -0600

    Testing authentication - it works!

--- api_testing.py
+++ api_testing.py
@@ -1,1 +1,17 @@
 import tweepy
+
+# Save a file containing the Key, Secret Key, and Token for Twitter access. Contains these variables and nothing else.
+from passwords import key, secret_key, token, secret_token
+
+
+# Testing authentication to Twitter
+auth = tweepy.OAuthHandler(key, secret_key)
+auth.set_access_token(token, secret_token)
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
