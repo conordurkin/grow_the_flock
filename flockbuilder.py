17213325d493c438b7c1ab57613b696b8f3a7bfd
Conor Durkin <conordurkin@me.com>
2021-01-11 12:06:15 -0600

    Added to do list in comments here

--- flockbuilder.py
+++ flockbuilder.py
@@ -8,3 +8,25 @@ auth.set_access_token(token, secret_token)
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+
+
+
+
+
+
+# THINGS TO DO:
+# 1. Store my 'base' list of followers and followed accounts.
+# 2. Generate a list of the people I'd like to try following.
+#     - Ideas: Suggested accounts, followers of followers, followers of followed, followed of followed, followed of followers, specific hashtags, specific bios?
+# 3. Record list of those people.
+# 4. Follow top 50 in a day
+# 5. Store the 50 new follows along with the date.
+# 6. Sleep for a day.
+# 7. Follow another 50, store in list, sleep.
+# 8. After [N] days, check list of followers for guys from step 5.
+#     If no -> unfollow. Log as unsuccessful.
+#     If yes -> stay following.
+# 9. Logic for who to follow should now check against the unsuccessful list too - don't wanna refollow anytime soon.
+# 
