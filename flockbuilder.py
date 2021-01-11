f487587112c8259fa7daeff1ed31600dd06a4a36
Conor Durkin <conordurkin@me.com>
2021-01-10 20:11:04 -0600

    Updated tokens to read/write access, also started building flockbuilder

--- flockbuilder.py
+++ flockbuilder.py
@@ -1,1 +1,10 @@
+# Import twitter, import relevant authentication codes from password file
 import tweepy
+from passwords import key, secret_key, token, secret_token
+
+#Authenticate to Twitter
+auth = tweepy.OAuthHandler(key, secret_key)
+auth.set_access_token(token, secret_token)
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
