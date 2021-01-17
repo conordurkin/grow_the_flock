5c055b0676687fe4b860970a6c6cbda13f5fd404
Conor Durkin <conordurkin@me.com>
2021-01-17 16:42:05 -0600

    Lots of progress! Just need to finish the unfollow-14day part

--- flockbuilder.py
+++ flockbuilder.py
@@ -1,78 +1,153 @@
+# This is going to be the file that runs daily and logs lots of info for me.
+
+
+### First Section: Basic Setup ###
+
 # Import twitter, import relevant authentication codes from password file
 import tweepy
 from passwords import key, secret_key, token, secret_token
 import pandas as pd
+from datetime import datetime as dt
+today = dt.today().strftime("%Y-%m-%d")
 
-#Authenticate to Twitter
+
+#Authenticate to Twitter and get API connected
 auth = tweepy.OAuthHandler(key, secret_key)
 auth.set_access_token(token, secret_token)
-
-# Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# Import lists of initial friends and followers
-initial_followers = pd.read_csv('initial_followers.csv')
-initial_friends = pd.read_csv('initial_friends.csv')
 
-initial_followers.columns = ['name']
-initial_friends.columns = ['name']
+### Second Section: Getting today's targets and following them ###
 
-initial_followers = list(initial_followers['name'])
-initial_friends = list(initial_friends['name'])
+# Read in our followed log (CSV / DataFrame) and our lists of potential targets (Pickles / Lists of Lists)
+followed_log = pd.read_csv('followed_log.csv')
+with open('mid_targets.pickle', 'rb') as f:
+    mid_targets = pickle.load(f)
+with open('top_targets.pickle', 'rb') as f:
+    top_targets = pickle.load(f)
+with open('low_targets.pickle', 'rb') as f:
+    low_targets = pickle.load(f)
+
+# Select today's targets and remove them from the lists.
+today_top_targets_temp = top_targets[0:50]
+today_mid_targets_temp = mid_targets[0:50]
+top_targets = top_targets[50:]
+mid_targets = mid_targets[50:]
+today_targets = []
+
+for target in today_top_targets_temp:
+    if target[0] in list(followed_log.id):
+        pass
+    else:
+        today_targets.append(target + ['top_target', today, ''])
+
+for target in today_mid_targets_temp:
+    if target[0] in list(followed_log.id):
+        pass
+    else:
+        today_targets.append(target + ['mid_target', today, ''])
+
+# Connect to Twitter and follow today's targets. Log them if we are able follow them.
+for target in today_targets:
+    try:
+        api.create_friendship(target[0])
+        new_row = pd.Series(target, index=followed_log.columns)
+        follower_log.append(new_row)
+
+    except:
+        pass
 
 
-##### Creating initial target set.
+### Third Section: Replenishing our Target Lists by pulling followers/friends from Today's Targets ###
 
-# First create lists of 'priority keywords' in Twitter bios/descriptions to classify potential targets.
+# Get list of follower IDs / friend IDs for the new people we're pulling.
+new_targets = []
+for target in today_targets:
+    name = target[0]
+    try:
+        user = api.get_user(name)
+        if user.followers_count <= 5000:
+            for follower in api.followers_ids(name):
+                new_targets.append(follower)
+        elif user.followers_count <= 100000:
+            call = tweepy.Cursor(api.followers_ids, id = name)
+            for page in call.pages():
+                for follower_id in page:
+                    new_targets.append(follower_id)
+        else:
+            pass
+
+        if user.friends_count <= 5000:
+            for friend in api.friends_ids(name):
+                new_targets.append(friend)
+        elif user.friends_count <= 100000:
+            call = tweepy.Cursor(api.friends_ids, id = name)
+            for page in call.pages():
+                for friend_id in page:
+                    new_targets.append(friend_id)
+        else:
+            pass
+
+    except:
+        pass
+
+# Filter down that list of new target IDs. Eliminate anyone we already have seen.
+new_targets = list(set(new_targets))
+old_targets = [target[0] for target in low_targets] + [target[0] for target in mid_targets] + [target[0] for target in top_targets]
+brand_new_targets = [target for targets in new_targets if target not in old_targets]
+
+# Keywords to classify accounts into one of the good lists.
 bio_top = ['chaplain', 'fr.', 'pastor', 'seminarian', 'priest', 'vicar', 'diocese']
 bio_mid = ['friar', 'catholic', 'jesuit', 'dominican', 'franciscan', 'parish']
 
+# Pull bios for anyone on the brand_new_targets lists.
+def divide_list(list, n):
+    for i in range(0, len(list), n):
+        yield list[i:(i+n)]
 
-# Read in information on our targets - this is a big dataset! We're going to pull bio information to classify them.
-with open('unique_targets_ids_info.pickle', 'rb') as f:
-    initial_targets = pickle.load(f)
+chunky_new_targets = list(divide_list(brand_new_targets, 100))
+new_target_info = []
 
-top_targets = []
-mid_targets = []
-low_targets = []
+for chunk in chunky_new_targets:
+    try:
+        users = api.lookup_users(chunk)
+        for user in users:
+            new_target_info.append(user.id, user.description)
+    except:
+        pass
 
-for account in initial_targets:
-    if any(word in account.description.lower() for word in bio_top):             # !!! Note: Need to get the syntax right to pull the account descriptions here...
+# Now determine which accounts go into which list.
+for account in new_target_info:
+    if any(word in account.description.lower() for word in bio_top):
         top_targets.append(account)
     elif any(word in account.description.lower() for word in bio_mid):
         mid_targets.append(account)
     else:
         low_targets.append(account)
 
-# Create a list of people we have already followed. Do I need to flag the initial guys somehow? Probably.
-already_followed = initial_followers.copy()
+# And store these target lists down as pickle files.
+with open('top_targets.pickle', 'wb') as f:
+    pickle.dump(top_targets, f)
+with open('mid_targets.pickle', 'wb') as f:
+    pickle.dump(mid_targets, f)
+with open('low_targets.pickle', 'wb') as f:
+    pickle.dump(low_targets, f)
+
+
+### Fourth Section: Unfollowing Accounts if they never follow us back ###
+lookback = (dt.today() - timedelta(14)).strftime("%Y-%m-%d")
+
+unfollow_candidates = followed_log.loc[followed_log.date == lookback]
+
+follower_list = []
+for follower in tweepy.Cursor(api.followers).items():
+    follower_list.append(follower.id)
 
 
 
+########################################################
+Still to do:
 
-
-
-
-
-
-
-
-What we want to do:
-1. Pull the top 100 'top targets' available.
-    See if they are in the already followed list.
-        if so, delete them and skip.
-        if not, follow them. Add their names to the already_followed list (along with date, screenname, user info, target list it came from).
-        if we can't follow them, throw them on an 'error' list of some kind.
-        Add them to a 'today' list as well
-
-2. Check our 'already followed' list for any names that are at least 14 days old.
         Check to see if they are our friend.
             If so, log them as a 'followed back!' somewhere.
             If not -> unfollow them.
-
-3. Pull all the IDs for the 'today' list friends + followers. Make sure we don't have too many names! Skip if over 100k.
-    Whittle this down to only things we haven't included in any target list or previous follows.
-    Pull all bios for whatever's left. Sort them into the appropriate target lists and append them onto the end.
-    Reset the 'today' list to a blank list.
-
-4. Sleep until tomorrow morning.
