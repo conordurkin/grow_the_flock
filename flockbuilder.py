2f290eb4499fda76f70e1874d6f5a3528e8d3191
Conor Durkin <conordurkin@me.com>
2021-01-21 16:57:32 -0600

    Commenting out the re-seeding part, some other changes.

--- flockbuilder.py
+++ flockbuilder.py
@@ -1,6 +1,6 @@
 # This is going to be the file that runs daily and logs lots of info for me.
 
-### First Section: Basic Setup ###
+##################### First Section: Basic Setup #####################
 
 # Import twitter, import relevant authentication codes from password file
 import tweepy
@@ -11,28 +11,38 @@ from datetime import datetime as dt
 from datetime import timedelta
 today = dt.today().strftime("%Y-%m-%d")
 
+# Paths to files you need to load go here.
+top_path = 'top_targets.pickle'
+mid_path = 'mid_targets.pickle'
+low_path = 'low_targets.pickle'
+log_path = 'followed_log.csv'
+
 #Authenticate to Twitter and get API connected
 auth = tweepy.OAuthHandler(key, secret_key)
 auth.set_access_token(token, secret_token)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-### Second Section: Getting today's targets and following them ###
+
+##################### Second Section: Getting today's targets and following them #####################
 
 # Read in our followed log (CSV / DataFrame) and our lists of potential targets (Pickles / Lists of Lists)
 followed_log = pd.read_csv('followed_log.csv')
-with open('mid_targets.pickle', 'rb') as f:
-    mid_targets = pickle.load(f)
-with open('top_targets.pickle', 'rb') as f:
+with open(top_path, 'rb') as f:
     top_targets = pickle.load(f)
-with open('low_targets.pickle', 'rb') as f:
+with open(mid_path, 'rb') as f:
+    mid_targets = pickle.load(f)
+with open(low_path, 'rb') as f:
     low_targets = pickle.load(f)
 
 # Select today's targets and remove them from the lists.
-today_top_targets_temp = top_targets[0:50]
-today_mid_targets_temp = mid_targets[0:50]
-top_targets = top_targets[50:]
-mid_targets = mid_targets[50:]
+for target in top_targets:
+    if target[0] in list(followed_log)
+
+today_top_targets_temp = top_targets[0:150]
+today_mid_targets_temp = mid_targets[0:150]
+top_targets = top_targets[150:]
+mid_targets = mid_targets[150:]
 today_targets = []
 
 for target in today_top_targets_temp:
@@ -57,83 +67,88 @@ for target in today_targets:
     except:
         pass
 
-### Third Section: Replenishing our Target Lists by pulling followers/friends from Today's Targets ###
 
-# Get list of follower IDs / friend IDs for the new people we're pulling.
-new_targets = []
-for target in today_targets:
-    name = target[0]
-    try:
-        user = api.get_user(name)
-        if user.followers_count <= 5000:
-            for follower in api.followers_ids(name):
-                new_targets.append(follower)
-        elif user.followers_count <= 100000:
-            call = tweepy.Cursor(api.followers_ids, id = name)
-            for page in call.pages():
-                for follower_id in page:
-                    new_targets.append(follower_id)
-        else:
-            pass
+####### Commenting out section 3 - taking too long to run and the target list is pretty long at this point ########
+# Note that I did this on 1/21/21 so anyone after then should be scraped in the future. 
 
-        if user.friends_count <= 5000:
-            for friend in api.friends_ids(name):
-                new_targets.append(friend)
-        elif user.friends_count <= 100000:
-            call = tweepy.Cursor(api.friends_ids, id = name)
-            for page in call.pages():
-                for friend_id in page:
-                    new_targets.append(friend_id)
-        else:
-            pass
+##################### Third Section: Replenishing our Target Lists by pulling followers/friends from Today's Targets #####################
+        #
+        # # Get list of follower IDs / friend IDs for the new people we're pulling.
+        # new_targets = []
+        # for target in today_targets:
+        #     name = target[0]
+        #     try:
+        #         user = api.get_user(name)
+        #         if user.followers_count <= 5000:
+        #             for follower in api.followers_ids(name):
+        #                 new_targets.append(follower)
+        #         elif user.followers_count <= 100000:
+        #             call = tweepy.Cursor(api.followers_ids, id = name)
+        #             for page in call.pages():
+        #                 for follower_id in page:
+        #                     new_targets.append(follower_id)
+        #         else:
+        #             pass
+        #
+        #         if user.friends_count <= 5000:
+        #             for friend in api.friends_ids(name):
+        #                 new_targets.append(friend)
+        #         elif user.friends_count <= 100000:
+        #             call = tweepy.Cursor(api.friends_ids, id = name)
+        #             for page in call.pages():
+        #                 for friend_id in page:
+        #                     new_targets.append(friend_id)
+        #         else:
+        #             pass
+        #
+        #     except:
+        #         pass
+        #
+        # # Filter down that list of new target IDs. Eliminate anyone we already have seen.
+        # new_targets = list(set(new_targets))
+        # old_targets = [target for target in low_targets] + [target[0] for target in mid_targets] + [target[0] for target in top_targets]
+        # brand_new_targets = [target for targets in new_targets if target not in old_targets]
+        #
+        # # Keywords to classify accounts into one of the good lists.
+        # bio_top = ['chaplain', 'fr.', 'pastor', 'seminarian', 'priest', 'vicar', 'diocese', 'deacon']
+        # bio_mid = ['friar', 'catholic', 'jesuit', 'dominican', 'franciscan', 'parish']
+        #
+        # # Pull bios for anyone on the brand_new_targets lists.
+        # def divide_list(list, n):
+        #     for i in range(0, len(list), n):
+        #         yield list[i:(i+n)]
+        #
+        # chunky_new_targets = list(divide_list(brand_new_targets, 100))
+        # new_target_info = []
+        #
+        # for chunk in chunky_new_targets:
+        #     try:
+        #         users = api.lookup_users(chunk)
+        #         for user in users:
+        #             new_target_info.append(user.id, user.description)
+        #     except:
+        #         pass
+        #
+        # # Now determine which accounts go into which list.
+        # for account in new_target_info:
+        #     if any(word in account.description.lower() for word in bio_top):
+        #         top_targets.append(account)
+        #     elif any(word in account.description.lower() for word in bio_mid):
+        #         mid_targets.append(account)
+        #     else:
+        #         low_targets.append(account[0]) #For low targets, only append the ID. Saves a ton of memory.
 
-    except:
-        pass
-
-# Filter down that list of new target IDs. Eliminate anyone we already have seen.
-new_targets = list(set(new_targets))
-old_targets = [target for target in low_targets] + [target[0] for target in mid_targets] + [target[0] for target in top_targets]
-brand_new_targets = [target for targets in new_targets if target not in old_targets]
-
-# Keywords to classify accounts into one of the good lists.
-bio_top = ['chaplain', 'fr.', 'pastor', 'seminarian', 'priest', 'vicar', 'diocese', 'deacon']
-bio_mid = ['friar', 'catholic', 'jesuit', 'dominican', 'franciscan', 'parish']
-
-# Pull bios for anyone on the brand_new_targets lists.
-def divide_list(list, n):
-    for i in range(0, len(list), n):
-        yield list[i:(i+n)]
-
-chunky_new_targets = list(divide_list(brand_new_targets, 100))
-new_target_info = []
-
-for chunk in chunky_new_targets:
-    try:
-        users = api.lookup_users(chunk)
-        for user in users:
-            new_target_info.append(user.id, user.description)
-    except:
-        pass
-
-# Now determine which accounts go into which list.
-for account in new_target_info:
-    if any(word in account.description.lower() for word in bio_top):
-        top_targets.append(account)
-    elif any(word in account.description.lower() for word in bio_mid):
-        mid_targets.append(account)
-    else:
-        low_targets.append(account[0]) #For low targets, only append the ID. Saves a ton of memory.
 
 # And store these target lists down as pickle files.
-with open('top_targets.pickle', 'wb') as f:
+with open(top_path, 'wb') as f:
     pickle.dump(top_targets, f)
-with open('mid_targets.pickle', 'wb') as f:
+with open(mid_path, 'wb') as f:
     pickle.dump(mid_targets, f)
-with open('low_targets.pickle', 'wb') as f:
+with open(low_path, 'wb') as f:
     pickle.dump(low_targets, f)
 
 
-### Fourth Section: Unfollowing Accounts if they never follow us back ###
+##################### Fourth Section: Unfollowing Accounts if they never follow us back #####################
 
 # Generate the list of IDs I need to look for.
 lookback = (dt.today() - timedelta(14)).strftime("%Y-%m-%d")
@@ -153,4 +168,4 @@ for id in unfollow_list:
         followed_log.loc[followed_log.id == id, 'flag'] = 0
         api.destroy_friendship(id)
 
-followed_log.to_csv('followed_log.csv', index = False)
+followed_log.to_csv(log_path, index = False)
